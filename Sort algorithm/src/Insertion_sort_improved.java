e557362bb78f5601be884fdac8749d45edc987cb
Yujia Lian <yujia.lian001@gmail.com>
2018-02-06 19:42:23 -0500

    o(n^2) sort algorithm

---
+++ Sort algorithm/src/Insertion_sort_improved.java
@@ -0,0 +1,11 @@
+/*For a nearly ordered／sorted array, insertion sort is much better than selection sort,
+ * even better than o(nlogn) algorithm like merge sort/quick sort.When it's orded array,
+ * intersion sort become o(n).
+Improved version: Don't need swap element, insertion sort can end inner loop earlier.*/
+public class Insertion_sort_improved {
+	private ins_imp(int[] arr) {
+		if(arr == null || arr.length == 0) {
+			
+		}
+	}
+}

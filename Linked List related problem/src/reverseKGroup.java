1bb402923fcd2a6040132cbb6f0a5795c3780fb7
Yujia Lian <yujia.lian001@gmail.com>
2018-02-27 15:10:28 -0500

    reverseKGroup recursively

--- Linked List related problem/src/reverseKGroup.java
+++ Linked List related problem/src/reverseKGroup.java
@@ -35,3 +35,27 @@ class Solution {
         return first;
     }
 }
+
+
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        int count = 0;
+        while(curr != null && count != k) {
+            curr = curr.next;
+            count++;
+        }
+        if(count == k) {
+            curr = reverseKGroup(curr, k);
+            while(count > 0) {
+                count--;
+                ListNode temp = head.next;
+                head.next = curr;
+                curr = head;
+                head = temp;
+            }
+            head = curr;
+        }
+        return head;
+    }
+}

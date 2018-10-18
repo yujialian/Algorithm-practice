9b3b622e5d7bb95c68a2f9f72f2d757cd6f2e827
Yujia Lian <yujialia@buffalo.edu>
2018-10-18 15:50:48 -0400

    Add comments, add Promise way to load the picture and compare with Async, Await.

--- JavaScript_Learn/src/index.js
+++ JavaScript_Learn/src/index.js
@@ -14,12 +14,23 @@ function loadImg(src) {
 	})
 	return promise
 }
+
 var src1 = 'https://www.imooc.com/static/img/index/logo.png'
 //var result1 = loadImg(src1)
 var src2 = 'https://img3.mukewang.com/54584ed2000152a202200220-100-100.jpg'
 //var result2 = loadImg(src2)
 
-//Async, await
+/*Promise way
+result1.then(function(img1) {
+    console.log('The first image is been loaded!', img1.width)
+    return result2
+}).then(function(img2) {
+    console.log('The second image is been loaded!', img2.width)
+}).catch(function(ex) {
+    console.log(ex)
+})*/
+//Async, await way, synchrnous way of writing.
+//Advantage: Don't need to pass call back function in then()
 const load = async function() {
     const result1 = await loadImg(src1)
     console.log(result1)

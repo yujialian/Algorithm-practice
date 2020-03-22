276ebda9d9c675831f4d953dc4b62bce4028ef06
Yujia Lian <yujialia@buffalo.edu>
2020-03-22 00:20:25 -0400

    Fix quick sort identical

--- Sort_Algorithm/src/quickSort/quickSortIdenticalKeys.java
+++ Sort_Algorithm/src/quickSort/quickSortIdenticalKeys.java
@@ -1,35 +1,32 @@
-public class quickSortIdenticalKeys {
-    //Two way
-    //When most of element in arr is identical, the divided array will be highly unblanced.
-    //We use 2 index, left index include the elements that is <= pivot, right index include the
-    //elements that is >= pivot. This kind of quick sort called: Dual pivot quicksort.
-    //Dual pivot Quicksort
-    public int partition(int[] arr, int l, int r) {
-        int v = arr[l];
-        int i = l + 1;
-        int j = r;
-        while(true) {
-            while(i <= r && arr[i] <= v) i++;
-            while(j >= l && arr[j] >= v) j--;
-            if(i > j) break;
-            swap(arr, i, j);
-            i++;
-            j--;
-        }
-        swap(arr, j, l);
-        return j;
-    }
-    public void quickIndenticalSort(int[] arr, int i, int j) {
-        int index = partition(arr, i, j);
-        quickIndenticalSort(arr, i, index - 1);
-        quickIndenticalSort(arr, index + 1, j);
-    }
-    public void quickSort(int[] arr) {
-        quickIndenticalSort(arr, 0, arr.length - 1);
-    }
-    public void swap(int[] array, int i, int j) {
-        int tmp = array[i];
-        array[i] = array[j];
-        array[j] = tmp;
-    }
+class quickSortIdenticalKeys {
+  public static int[] quickSort(int[] array) {
+		if (array == null || array.length == 0) return new int[0];
+		__quickSort(array, 0, array.length - 1);
+		return array;
+  }
+	private static void __quickSort(int[] array, int l, int r) {
+		if (l >= r) return;
+		int p = __partition(array, l, r);
+		__quickSort(array, l, p - 1);
+		__quickSort(array, p + 1, r);
+	}
+	private static int __partition(int[] array, int l, int r) {
+		int p = array[l];
+		int left = l + 1, right = r;
+		while (true) {
+			while (left <= r && array[left] < p) left++; 
+			while (right >= l && array[right] > p) right--;
+			if (left > right) break;
+			swap(array, left, right);
+			left++;
+			right--;
+		}
+		swap(array, right, l);
+		return right;
+	}
+	private static void swap(int[] array, int l, int r) {
+		int tmp = array[l];
+		array[l] = array[r];
+		array[r] = tmp;
+	}
 }
